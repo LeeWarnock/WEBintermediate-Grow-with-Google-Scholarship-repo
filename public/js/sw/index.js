86ed40f2ff783473d3aed7d1e265a2b4466bb970
Lee Warnock <clwarnock@gmail.com>
2018-01-25 22:08:16 -0600

    reset index.js for practice

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,13 +2,5 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-
-	console.log("Caught a fetch!");
-
-	event.respondWith(
-    new Response("<i class='a-winner-is-me'>Hii</i>", {
-        headers: { 'Content-Type': 'text/html' }
-      }
-    )
-  );
+  console.log(event.request);
 });
