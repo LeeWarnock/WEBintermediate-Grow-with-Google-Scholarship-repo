6388edaf6b91e2998491b1a6cd7f7133e92d9afc
Lee Warnock <clwarnock@gmail.com>
2018-01-25 21:15:50 -0600

    add HTML to event listener

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,8 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
+
+	<p>Hey</p>
+
   console.log(event.request);
 });
